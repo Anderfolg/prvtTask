34a826f03e07465d961e8476f4168ec86a79f7fd
anderfolg <anderfolglive@gmail.com>
2024-10-01 16:25:42 +0300

    refactored properties and etcc

--- business-rest/src/main/java/com/anderfolg/businessrest/BusinessRestApplication.java
+++ business-rest/src/main/java/com/anderfolg/businessrest/BusinessRestApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
-@PropertySource("classpath:application-business.yml")
+@PropertySource("classpath:application.properties")
 public class BusinessRestApplication {
 
     public static void main( String[] args ) {
